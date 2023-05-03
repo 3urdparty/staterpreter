09964b5d8db5343a8620fa79cf264aaa67cdeec5
3urd Party <3urdparty@gmail.com>
2023-05-03 20:36:17 +0800

    Changed some lines

--- src/commands/subCMD.cpp
+++ src/commands/subCMD.cpp
@@ -1,14 +1,13 @@
+#include <filuzzy/filuzzy.hpp>
 #include <iomanip>
 #include <iostream>
+#include <statsi/statsi.hpp>  // library of statistical functions to be used in program written by Mubarak
+#include <strfmt/strfmt.hpp>  // library of simple generic functions Mustafa and Azi wrote to be used in the main program. Source code found at libs/strfmt
+#include <tabluzzy/tabluzzy.hpp>  // library containing a class for the Table to be used in this Program written by Mustafa
+#include <terminalcancer/terminalcancer.hpp>  // library of simple terminal helper functions to be used in program written by Mustafa
 #include <vector>
 
 #include "commands.hpp"  // Header file for all the interpreter commands for staterpreter
-
-#include <strfmt/strfmt.hpp>  // library of simple generic functions Mustafa and Azi wrote to be used in the main program. Source code found at libs/strfmt
-#include <tabluzzy/tabluzzy.hpp>  // library containing a class for the Table to be used in this Program written by Mustafa
-#include <filuzzy/filuzzy.hpp>
-#include <statsi/statsi.hpp>  // library of statistical functions to be used in program written by Mubarak
-#include <terminalcancer/terminalcancer.hpp>  // library of simple terminal helper functions to be used in program written by Mustafa
 using namespace std;
 void subCMD(vector<string>& args, Table& currentTable, bool& tableLoaded) {
   if (tableLoaded) {
@@ -54,7 +53,7 @@ void subCMD(vector<string>& args, Table& currentTable, bool& tableLoaded) {
             float value;
             for (int y = 0; y < col1_values.size(); y++) {
               value = col1_values[y] - col2_values[y];
-              cout << setw(8) << setfill(' ') << "|"   << setprecision(0) << fixed
+              cout << setw(8) << setfill(' ') << "|" << setprecision(0) << fixed
                    << value << "\t"
                    << "|";
 
